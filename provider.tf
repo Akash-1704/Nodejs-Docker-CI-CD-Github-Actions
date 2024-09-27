68efec73087e33bddcea0e374416266e8d14c33e
Akash <akashdurai1704@gmail.com>
2024-09-28 04:28:55 +0530

    first commit

---
+++ provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  cloud {
+    organization = "skyfire-1704"
+    workspaces {
+      name = "ecs-fargate-github"
+    }
+  }
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+provider "aws" {
+  region = var.aws_region
+  # profile = var.aws_profile
+
+}
