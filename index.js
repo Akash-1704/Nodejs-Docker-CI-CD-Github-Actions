374a5a8cf468d25f680e7d8e2039b55972d21ebe
Akash <akashdurai1704@gmail.com>
2024-09-29 08:03:10 +0530

    testing unit tests file

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 var express = require('express');
 var app = express();
+var server;
 
 app.set("port", process.env.PORT || 80);
 const greetingMessage = process.env.GREETING || 'Hello to Cloud & DevOps World !!!!';
@@ -17,13 +18,13 @@ app.get('/', function(req, res){
 
 */
 const PORT = process.env.NODE_ENV === 'test' ? 3000 : (process.env.PORT || 80);
-var server = app.listen(PORT, function () {
-  
-  var host = server.address().address
-  var port = server.address().port
 
-  console.log("Node.js API app listening at http://%s:%s", host, port)
-
-})
+if (!module.parent) {
+  server = app.listen(PORT, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log("Node.js API app listening at http://%s:%s", host, port);
+  });
+}
 
 module.exports = app; // Export app for testing
