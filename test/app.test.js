cf83881ce03f61a25d0bad99acc6a1927b24cd52
Akash <akashdurai1704@gmail.com>
2024-09-29 08:05:31 +0530

    testing unit tests file

--- test/app.test.js
+++ test/app.test.js
@@ -1,6 +1,12 @@
 const request = require('supertest');
 const app = require('../index');
 
+afterAll((done) => {
+  server.close(); // Close the server after tests
+  done();
+});
+
+
 describe('GET /', () => {
   it('responds with the correct greeting message', async () => {
     const expectedMessage = process.env.GREETING || 'Hello to Cloud & DevOps World !!!!';
